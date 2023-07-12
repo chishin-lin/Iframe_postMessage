36d88bd79a8fa5c5648435786ef54dc3e4cbef8d
Queena <queena@innotech.me>
2023-07-12 10:25:33 +0800

    version.01

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  useEffect(() => {
+    const closePage = (event: any) => {
+      console.log('來源', event.origin);
+      if (event.origin === window.location.origin || event.origin === 'https://kf4.jpcaqc.com') {
+        console.log('event.data:::', event.data);
+
+        // 只有返回backStatus为true时才关闭客服页面
+        if (event.data.backStatus) {
+          // 需要关闭的操作
+          console.log(':需要关闭了^_^');
+        }
+      }
+    };
+    window.addEventListener('message', closePage);
+    return () => {
+      window.removeEventListener('message', closePage);
+    };
+  }, []);
+
+  const handleClick = () => {
+    const iframeElement = iframeRef.current;
+    if (iframeElement) {
+      console.log(':::so slow');
+      iframeElement?.contentWindow?.postMessage({ isShowEnd: true, back: true }, '*');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={handleClick}>反回</button>
+      <iframe ref={iframeRef} src='' title='customerService' />
     </div>
   );
 }
